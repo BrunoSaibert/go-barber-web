c46371e416fff0afcb96fb77218ce1d39aa0455d
BrunoSaibert <brunosaibert@gmail.com>
2020-05-31 09:43:35 -0300

    :pencil: Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const src: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <SignUp />
       <GlobalStyle />
     </>
   );
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import * as S from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Background />
+
+      <S.Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input type="text" name="name" placeholder="Nome" icon={FiUser} />
+
+          <Input type="email" name="email" placeholder="E-mail" icon={FiMail} />
+
+          <Input
+            type="password"
+            name="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+
+          <Button type="button">Cadastrar</Button>
+        </form>
+
+        <a href="/">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </S.Content>
+    </S.Container>
+  );
+};
+
+export default SignUp;
