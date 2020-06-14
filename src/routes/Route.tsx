32f082443ec604c075ee779c19ae1659df4f80c3
BrunoSaibert <brunosaibert@gmail.com>
2020-06-13 23:00:41 -0300

    :sparkles: Rotas autenticadas

---
+++ src/routes/Route.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/AuthContext';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...props
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...props}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
